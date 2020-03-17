7160fc8ca544d0cd9044ba8dd218503d8592c2b8
Felipe Loureiro <felipe-loureiro@outlook.com>
2020-03-16 23:15:35 -0300

    First commit

---
+++ src/main/java/br/edu/ifsp/spo/bulls/bbooks/BbooksApplication.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.bulls.bbooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbooksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbooksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/ifsp/spo/bulls/bbooks/BbooksApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.ifsp.spo.bulls.bbooks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbooksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
