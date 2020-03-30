f5208de957fafcc5ad4482139b4ed135ae001549
Felipe Loureiro <felipe-loureiro@outlook.com>
2020-03-29 23:58:06 -0300

    multi project grandle

---
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/UsersApiApplication.java
@@ -0,0 +1,11 @@
+package br.edu.ifsp.spo.bulls.usersApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UsersApiApplication.class, args);
+    }
+}
