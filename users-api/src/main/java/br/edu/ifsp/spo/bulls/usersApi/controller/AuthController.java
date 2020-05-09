1c46bd5ea70c079f4c76573787a5e666a75f1f20
Felipe Loureiro <felipe-loureiro@outlook.com>
2020-05-08 21:46:55 -0300

    auth login

---
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/controller/AuthController.java
@@ -0,0 +1,10 @@
+package br.edu.ifsp.spo.bulls.usersApi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+}
