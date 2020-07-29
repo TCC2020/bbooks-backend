bcd05627072561cc22bec781f4f0e89614c9566e
Felipe Loureiro <felipe-loureiro@outlook.com>
2020-07-28 21:17:19 -0400

    fixing

--- users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/domain/UserBooks.java
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/domain/UserBooks.java
@@ -25,7 +25,10 @@ public class UserBooks {
     public enum Status {
         QUERO_LER("Quero ler"),
         LENDO("Lendo"),
-        LIDO("Lido");
+        LIDO("Lido"),
+        EMPRESTADO("Emprestado"),
+        RELENDO("Relendo"),
+        INTERROMPIDO("INTERROMPIDO");
 
         private final String text;
         Status(final String text) {
