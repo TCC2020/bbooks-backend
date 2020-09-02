ff520a81b039791ad5188ed09539ee6796b5410d
nessaoana <nessaoana09@gmail.com>
2020-09-01 22:21:49 -0300

    Adicionar userName no update

--- users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/service/UserService.java
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/service/UserService.java
@@ -95,6 +95,7 @@ public class UserService{
 		
 		return beanUtil.toUserTO(rep.findById(user.getId()).map( user1 -> {
 			user1.setEmail(user.getEmail());
+			user1.setUserName(user.getUserName());
 			return rep.save(user);
 		}).orElseThrow( () -> new ResourceNotFoundException("User not found")));
 		
