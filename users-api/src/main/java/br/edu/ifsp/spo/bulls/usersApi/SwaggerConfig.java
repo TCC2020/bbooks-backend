e156b62da5ebebe3d425e09c543962f731fe81c0
nessaoana <nessaoana09@gmail.com>
2020-09-20 18:12:12 -0300

    Configurando swagger para aparecer apenas os controllers do projeto

--- users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/SwaggerConfig.java
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/SwaggerConfig.java
@@ -16,7 +16,7 @@ public class SwaggerConfig {
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.any())
+                .apis(RequestHandlerSelectors.basePackage("br.edu.ifsp.spo.bulls"))
                 .paths(PathSelectors.any())
                 .build();
     }
