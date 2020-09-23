cfed5d42befea9f21eb8ed1dc42927d5adbfca51
nessaoana <nessaoana09@gmail.com>
2020-09-22 22:00:54 -0300

    Configuracoes de log, gravando em um arquivo

--- users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/controller/LoggingController.java
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/controller/LoggingController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class LoggingController {
 
-    Logger logger = LoggerFactory.getLogger(LoggingController.class);
+    private Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
     @RequestMapping("/")
     public String index() {
