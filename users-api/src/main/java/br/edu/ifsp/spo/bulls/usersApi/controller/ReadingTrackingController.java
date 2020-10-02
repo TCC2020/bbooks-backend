e4def944b1aca0ae77e8a095dc4ed6c2a5830d56
nessaoana <nessaoana09@gmail.com>
2020-10-02 09:54:08 -0300

    Criando as rotas

--- users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/controller/ReadingTrackingController.java
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/controller/ReadingTrackingController.java
@@ -1,11 +1,86 @@
 package br.edu.ifsp.spo.bulls.usersApi.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.edu.ifsp.spo.bulls.usersApi.domain.ReadingTracking;
+import br.edu.ifsp.spo.bulls.usersApi.dto.UserTO;
+import br.edu.ifsp.spo.bulls.usersApi.service.ReadingTrackingService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashSet;
+import java.util.UUID;
 
 @RestController
 @RequestMapping(value = "/tracking", produces="application/json")
 @CrossOrigin(origins = "*")
 public class ReadingTrackingController {
+
+    private Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @Autowired
+    ReadingTrackingService service;
+
+    @ApiOperation(value = "Retorna todos acompanhamentos de um livro")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna uma lista de acompanhamentos")
+    })
+    @GetMapping("/{userBook}")
+    public HashSet<ReadingTracking> getAllByBook(@PathVariable Long userBook){
+        logger.info("Acessando dados de todos os acompanhamento do userBook: " + userBook);
+        HashSet<ReadingTracking> acompanhamntos = service.getAllByBook(userBook);
+        logger.info("Acompanhamentos retornados: " + acompanhamntos.toString());
+        return acompanhamntos ;
+    }
+
+    @ApiOperation(value = "Retorna todos acompanhamentos de um livro")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Retorna um acompanhamento")
+    })
+    @GetMapping("/{readingTracking}")
+    public ReadingTracking getOne(@PathVariable Long readingTracking){
+        logger.info("Acessando dados de um acompanhamento");
+        ReadingTracking acompanhamento = service.get(readingTracking);
+        logger.info("Acompanhamento retornado: " + acompanhamento.toString());
+        return acompanhamento ;
+    }
+
+    @ApiOperation(value = "Cadastra um acompanhamento de leitura")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Acompanhamento cadastrado")
+    })
+    @PostMapping()
+    public ReadingTracking post(@RequestBody ReadingTracking readingTracking){
+        logger.info("Cadastrando um novo acompanhamento: " + readingTracking);
+        ReadingTracking acompanhamento = service.save(readingTracking);
+        logger.info("Acompanhamento cadastrado: " + acompanhamento.toString());
+        return acompanhamento ;
+    }
+
+    @ApiOperation(value = "Altera um acompanhemnto")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Acompanhamento atualizado")
+    })
+    @PutMapping("/trackingID")
+    public ReadingTracking put(@RequestBody ReadingTracking readingTracking, @PathVariable UUID trackingID){
+        logger.info("Alterando um acompanhamento: " + readingTracking);
+        ReadingTracking acompanhamento = service.update(readingTracking, trackingID);
+        logger.info("Acompanhamento alterado: " + acompanhamento.toString());
+        return acompanhamento ;
+    }
+
+    @ApiOperation(value = "Deleta um acompanhemnto")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Acompanhamento deletado")
+    })
+    @DeleteMapping("/trackingID")
+    public ReadingTracking delete(@PathVariable UUID trackingID){
+        logger.info("Deletando o acompanhamento: " + trackingID);
+        ReadingTracking acompanhamento = service.delete(trackingID);
+        logger.info("Acompanhamento deletado: " + acompanhamento.toString());
+        return acompanhamento ;
+    }
 }
