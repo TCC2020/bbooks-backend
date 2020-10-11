50b758a3a444084e17785501a67e25e62e48ea01
nessaoana <nessaoana09@gmail.com>
2020-10-11 16:58:53 -0300

    Adicionando código de erro no BookService

--- users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/enums/CodeException.java
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/usersApi/enums/CodeException.java
@@ -7,7 +7,10 @@ public enum CodeException {
     AT001("Error, wrong password or user not found", "AT001"),
     AT002("Error, token not found", "AT002"),
     AT003("Password already changed", "AT003"),
-    US001("User not found", "US001");
+    US001("User not found", "US001"),
+    BK001("IBNB already exists", "BK001"),
+    BK002("Book not found", "BK002"),
+    BK003("Book should have at least 1 author", "BK003");
 
 
 
