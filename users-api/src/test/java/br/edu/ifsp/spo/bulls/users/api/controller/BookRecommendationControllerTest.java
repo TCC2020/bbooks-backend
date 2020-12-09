e8553449e3ac33af952d2d119d6299132fdf93e8
nessaoana <nessaoana09@gmail.com>
2020-12-09 20:08:18 -0300

    imports genericos

--- users-api/src/test/java/br/edu/ifsp/spo/bulls/users/api/controller/BookRecommendationControllerTest.java
+++ users-api/src/test/java/br/edu/ifsp/spo/bulls/users/api/controller/BookRecommendationControllerTest.java
@@ -15,7 +15,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
