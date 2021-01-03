a0517ce637c5dfedba9a7c268532643a3a141460
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-01-03 14:02:02 -0300

    competition-api

---
+++ competition-api/src/test/java/br/edu/ifsp/spo/bulls/users/api/FeedApiApplicationTests.java
@@ -0,0 +1,18 @@
+package br.edu.ifsp.spo.bulls.users.api;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+public class FeedApiApplicationTests {
+
+    @Test
+    public void test() {
+        assertEquals(1, 1);
+    }
+}
