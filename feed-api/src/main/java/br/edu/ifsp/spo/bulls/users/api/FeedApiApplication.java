6f7675065243a636ceb6ce026de750ce69e1533d
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-01-03 12:57:22 -0300

    build fix

--- feed-api/src/main/java/br/edu/ifsp/spo/bulls/users/api/FeedApiApplication.java
+++ feed-api/src/main/java/br/edu/ifsp/spo/bulls/users/api/FeedApiApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UsersApiApplication {
+public class FeedApiApplication {
     public static void main(String[] args) {
-        SpringApplication.run(UsersApiApplication.class, args);
+        SpringApplication.run(FeedApiApplication.class, args);
     }
 }
