2b14f23da9ba4d773a9196c6ec3bb27146c29b9d
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-01-03 16:41:52 -0300

    push lint

--- competition-api/src/main/java/br/edu/ifsp/spo/bulls/users/api/controller/CompetitionController.java
+++ competition-api/src/main/java/br/edu/ifsp/spo/bulls/users/api/controller/CompetitionController.java
@@ -6,7 +6,10 @@ import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(value = "/competitions", produces="application/json")
