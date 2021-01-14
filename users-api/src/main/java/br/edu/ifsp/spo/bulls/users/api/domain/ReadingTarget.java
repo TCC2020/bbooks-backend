6d39bf9537c7df4c4c773c5370156c75e0dfc5eb
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-01-14 12:58:28 -0300

    fix

--- users-api/src/main/java/br/edu/ifsp/spo/bulls/users/api/domain/ReadingTarget.java
+++ users-api/src/main/java/br/edu/ifsp/spo/bulls/users/api/domain/ReadingTarget.java
@@ -6,7 +6,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
+import javax.persistence.ManyToMany;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+
 import java.util.List;
 import java.util.UUID;
 
