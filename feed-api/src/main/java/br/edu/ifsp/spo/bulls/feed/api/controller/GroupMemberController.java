6724f817afe19ca326425cb8b7161656ff0808d9
nessaoana <nessaoana09@gmail.com>
2021-01-30 18:10:02 -0300

    adicionar e excluir membro

--- feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/controller/GroupMemberController.java
+++ feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/controller/GroupMemberController.java
@@ -1,11 +1,14 @@
 package br.edu.ifsp.spo.bulls.feed.api.controller;
 
 import br.edu.ifsp.spo.bulls.feed.api.domain.Group;
+import br.edu.ifsp.spo.bulls.feed.api.domain.GroupMembers;
+import br.edu.ifsp.spo.bulls.feed.api.service.GroupMemberService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,14 +18,18 @@ public class GroupMemberController {
     
     private final Logger logger = LoggerFactory.getLogger(GroupMemberController.class);
 
+    @Autowired
+    private GroupMemberService service;
+
     @ApiOperation(value = "Entrar em um grupo")
     @ApiResponses( value = {
             @ApiResponse(code = 200, message = "Membro adicionado")
     })
     @PostMapping
-    public void enterGroup(@RequestBody Group group) {
+    public void enterGroup(@RequestBody GroupMembers member) {
         // TODO: Authorization e identificação da pessoa para diferenciar membro ou não - Para adicionar admin
-        // TODO: Chamar o service aqui
+
+        service.putMember(member);
     }
 
     @ApiOperation(value = "Sair de um grupo")
@@ -30,9 +37,9 @@ public class GroupMemberController {
             @ApiResponse(code = 200, message = "Membro retirado do grupo")
     })
     @DeleteMapping
-    public void exitGroup(@RequestBody Group group) {
+    public void exitGroup(@RequestBody GroupMembers member) {
         // TODO: Authorization e identificação da pessoa - Para excluir outras pessoas do grupo (só para o dono e admin)
-        // TODO: Chamar o service aqui
+        service.exitMember(member);
     }
 
 }
--- feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/service/GroupMemberService.java
+++ feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/service/GroupMemberService.java
@@ -1,4 +1,21 @@
 package br.edu.ifsp.spo.bulls.feed.api.service;
 
+import br.edu.ifsp.spo.bulls.feed.api.domain.GroupMembers;
+import br.edu.ifsp.spo.bulls.feed.api.repository.GroupMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class GroupMemberService {
+
+    @Autowired
+    private GroupMemberRepository repository;
+
+    public void putMember(GroupMembers membro){
+        repository.save(membro);
+    }
+
+    public void exitMember(GroupMembers membro){
+        repository.deleteById(membro.getId());
+    }
 }
