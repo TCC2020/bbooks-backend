b5a70cb01bcefae83bcf0aae85ff79b89d582a18
nessaoana <nessaoana09@gmail.com>
2021-02-04 22:23:49 -0300

    imports n utilizados

--- feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/controller/GroupMemberController.java
+++ feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/controller/GroupMemberController.java
@@ -5,8 +5,6 @@ import br.edu.ifsp.spo.bulls.feed.api.service.GroupMemberService;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiResponse;
 import io.swagger.annotations.ApiResponses;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
