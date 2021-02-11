b37f6dbb912493948b708f9ed99d096c6130182d
Felipe Loureiro <42723584+feloureiro@users.noreply.github.com>
2021-02-11 19:53:32 -0300

    Update PostBeanUtil.java

--- feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/bean/PostBeanUtil.java
+++ feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/bean/PostBeanUtil.java
@@ -7,7 +7,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
