23c5f7024c222245f1e10ee203135b8e8e1182c6
Felipe Loureiro <felipe-loureiro@outlook.com>
2021-02-20 22:50:33 -0300

    push cdn

---
+++ common/src/main/java/br/edu/ifsp/spo/bulls/common/api/enums/CDNEnum.java
@@ -0,0 +1,25 @@
+package br.edu.ifsp.spo.bulls.common.api.enums;
+
+public enum CDNEnum {
+    profile_image("PROFILE_IMAGE"),
+    book_ad_id("BOOK_AD_ID"),
+    book_image("BOOK_IMAGE");
+
+    private final String text;
+    CDNEnum(final String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
+    public static CDNEnum getByString(String value) {
+        for(CDNEnum objectType : CDNEnum.values()){
+            if(objectType.text.equalsIgnoreCase(value))
+                return objectType;
+        }
+        return null;
+    }
+}
---
+++ feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/controller/CDNController.java
@@ -0,0 +1,69 @@
+package br.edu.ifsp.spo.bulls.feed.api.controller;
+
+import br.edu.ifsp.spo.bulls.common.api.enums.CDNEnum;
+import br.edu.ifsp.spo.bulls.feed.api.feign.CompetitionCommonFeign;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.storage.BlobInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Map;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/cdn")
+@CrossOrigin(origins = "*")
+public class CDNController {
+    private final ObjectMapper mapper = new ObjectMapper();
+
+    private Storage storage = StorageOptions.getDefaultInstance().getService();
+
+    @Autowired
+    private CompetitionCommonFeign competitionCommonFeign;
+
+//    Storage storage = StorageOptions.newBuilder()
+//            .setCredentials(ServiceAccountCredentials.fromStream(new FileInputStream("C:/Users/AndreNascimentodeFre/Downloads/key.json")))
+//            .build()
+//            .getService();
+
+    public CDNController() throws IOException {
+    }
+
+    @PostMapping(value = "/upload", consumes = {"multipart/form-data"})
+    public HttpStatus upload(
+            @RequestHeader(value = "AUTHORIZATION") String token,
+            @RequestPart(value = "info", required = true) String info,
+            @RequestPart(value = "file", required = true) MultipartFile file) throws IOException {
+        Map<String, Object> infoMap = mapper.readValue(info, new TypeReference<Map<String, Object>>() {
+        });
+        String fileName = file.getOriginalFilename();
+
+        try {
+            BlobInfo blobInfo = storage.create(
+                    BlobInfo.newBuilder("cdn-bbooks", file.getOriginalFilename()).build(), //get original file name
+                    file.getBytes(), // the file
+                    Storage.BlobTargetOption.predefinedAcl(Storage.PredefinedAcl.PUBLIC_READ) // Set file permission
+            );
+            return this.handleUpload(blobInfo.getMediaLink(), StringUtils.removeStart(token, "Bearer").trim(), infoMap); // Return file url
+        }catch(IllegalStateException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    private HttpStatus handleUpload(String url, String token, Map<String, Object>infoMap) {
+        CDNEnum objectType = CDNEnum.getByString((String) infoMap.get("objectType"));
+        switch (objectType) {
+            case book_ad_id :
+                return competitionCommonFeign.uploadImage(url, (UUID) infoMap.get("bookAdId"), token);
+            default:
+                return HttpStatus.CONFLICT;
+        }
+    }
+}
