71a05a48e58ac330d068232a4f3036fb7f5233d2
nessaoana <nessaoana09@gmail.com>
2021-03-17 19:12:17 -0300

    fix: BookMonth não estava retornando groupId e nem salvando o group

--- feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/bean/BookMonthBeanUtil.java
+++ feed-api/src/main/java/br/edu/ifsp/spo/bulls/feed/api/bean/BookMonthBeanUtil.java
@@ -1,13 +1,17 @@
 package br.edu.ifsp.spo.bulls.feed.api.bean;
 
 import br.edu.ifsp.spo.bulls.common.api.dto.BookMonthTO;
+import br.edu.ifsp.spo.bulls.common.api.enums.CodeException;
+import br.edu.ifsp.spo.bulls.common.api.exception.ResourceNotFoundException;
 import br.edu.ifsp.spo.bulls.feed.api.domain.BookMonth;
+import br.edu.ifsp.spo.bulls.feed.api.domain.GroupRead;
+import br.edu.ifsp.spo.bulls.feed.api.repository.GroupRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import java.awt.print.Book;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -16,10 +20,17 @@ public class BookMonthBeanUtil {
 
     private Logger logger = LoggerFactory.getLogger(BookMonthBeanUtil.class);
 
+    @Autowired
+    private GroupRepository groupRepository;
+
     public BookMonth toDomain(BookMonthTO monthTO){
         BookMonth bookMonth = new BookMonth();
         try{
             BeanUtils.copyProperties(monthTO, bookMonth);
+
+            GroupRead groupRead = groupRepository.findById(monthTO.getId())
+                    .orElseThrow(() -> new ResourceNotFoundException(CodeException.GR001.getText(), CodeException.GR001));
+            bookMonth.setGroup(groupRead);
         }catch(Exception e) {
             logger.error("Error while converting BookMonthTO to BookMonth: " +  e);
         }
@@ -30,6 +41,7 @@ public class BookMonthBeanUtil {
         BookMonthTO bookMonthTO = new BookMonthTO();
         try{
             BeanUtils.copyProperties(bookMonth, bookMonthTO);
+            bookMonthTO.setGroupId(bookMonth.getGroup().getId());
         }catch(Exception e) {
             logger.error("Error while converting BookMonth to BookMonthTO: " +  e);
         }
